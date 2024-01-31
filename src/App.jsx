041c401a7931136db7bc02b0046a267e889dd121
shaikot07 <saifulislamshaikat007@gmail.com>
2024-02-01 00:09:31 +0600

    create store by redux and redux thank as middlewer

--- src/App.jsx
+++ src/App.jsx
@@ -15,3 +15,12 @@ function App() {
 }
 
 export default App
+
+// step 1: install packages
+// step 2: constans define
+// step 3: asynce action creator 
+// step 4: reducer 
+// step 5: create store 
+// step 6 : set provider in main jsx
+// step 7: use store 
+// step 8: adding css for style 
